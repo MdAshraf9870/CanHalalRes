d8d12584bc0a63f523a9069c49df5e7c1783da8b
mdash <mdashraf9870@gmail.com>
2021-07-30 00:18:12 +0530

    fixs

---
+++ app/src/main/java/com/e/canhalalres/Activity/RestaurantDetail/RestaurantDetail.java
@@ -0,0 +1,26 @@
+package com.e.canhalalres.Activity.RestaurantDetail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.e.canhalalres.R;
+
+public class RestaurantDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_restaurant_detail);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+       }
+
+    }
+}
